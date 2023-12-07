4357a9c31b8548788be0d23f8632703f9a49b40e
ajmir khan <127777945+ajmircoder@users.noreply.github.com>
2023-12-07 13:00:21 +0530

    Create subSet.js

---
+++ subSet.js
@@ -0,0 +1,20 @@
+//javascript
+
+let arr = [1, 2, 3, 4, 5];
+let subArr = [];
+const printArry = (l, r, arr)=>{
+    let arr1 = [];
+    for(let i = l; i <= r; i++){
+        arr1.push(arr[i])
+    }
+    subArr.push(arr1);
+    // console.log(arr1);
+}
+const subSet = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            printArry(i, j, arr)
+        }
+    }
+}
+subSet(arr);
