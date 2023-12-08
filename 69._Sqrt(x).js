9a803709f3db5db78b0a48dc90875b43fb794796
ajmir khan <127777945+ajmircoder@users.noreply.github.com>
2023-12-08 12:49:25 +0530

    Create 69._Sqrt(x).js

---
+++ 69._Sqrt(x).js
@@ -0,0 +1,12 @@
+// 69. Sqrt(x)
+//https://leetcode.com/problems/sqrtx/description/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+// solution- 1
+var mySqrt = function(x) {
+    return Math.floor(Math.pow(x, 1/2));
+};
