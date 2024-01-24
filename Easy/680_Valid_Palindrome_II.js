a691110a033d81ca62a5e2e11b6f7c5196551777
ajmir khan <127777945+ajmircoder@users.noreply.github.com>
2024-01-24 17:45:26 +0530

    Update 680_Valid_Palindrome_II.js

--- Easy/680_Valid_Palindrome_II.js
+++ Easy/680_Valid_Palindrome_II.js
@@ -10,21 +10,36 @@ var validPalindrome = function(s) {
         if(s[l] == s[r]){
             l++
             r--
-        }else{
-            return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1);
-        }
+        }else return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1);
     }
     return true;
 };
-
 function isPalindrome(s, l, r){
     while(l < r){
         if(s[l] == s[r]){
             l++
             r--
-        }else{
-            return false;
-        }
+        }else return false;
     }
     return true;
 }
+
+// Solution: 2;
+// Time-complexity: O(n^2)
+// Space-complexity: O(1);
+var validPalindrome = function(s) {
+    for(let i = 0; i < s.length; i++){
+        let palindrome = (s.substring(0, i) + s.substring(i + 1, s.length));
+        let l = 0, r = palindrome.length - 1, flag = true;
+        while(l < r){
+            if(palindrome[l] !== palindrome[r]){
+                flag = false;
+                break;
+            }
+            l++
+            r--
+        }
+        if(flag) return flag;
+    }
+    return false;
+};
