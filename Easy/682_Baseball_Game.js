614c7f29197907f175c28d48ecbfae0e3d5bbfe9
ajmir khan <127777945+ajmircoder@users.noreply.github.com>
2024-02-22 14:46:09 +0530

    Create 682_Baseball_Game.js

---
+++ Easy/682_Baseball_Game.js
@@ -0,0 +1,23 @@
+// 682.Baseball Game
+// https://leetcode.com/problems/baseball-game/description/
+
+// Solution: 1;
+// Time-Complexity: O(n);
+// Space-Complexity: O(1);
+var calPoints = function(operations) {
+    let ans = [];
+    for(let op of operations){
+        if(op == "+"){
+            ans.push(ans[ans.length-1]+ans[ans.length-2]);
+        }else if(op == "D"){
+            let pvr = ans[ans.length-1];
+            ans.push(pvr*2);
+        }else if(op == "C"){
+            ans.pop();
+        }else{
+            ans.push(Number(op));
+        }
+    }
+    return ans.reduce((accumulator, currentValue) => accumulator + currentValue,0);
+    
+};
